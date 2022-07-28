728dfb10b8499de275758fabb6273856338c9d74
Kananek Thongkam <info.dvgamer@gmail.com>
2022-07-28 23:15:12 +0700

    [Fix] Build assests.

--- vite.config.js
+++ vite.config.js
@@ -6,14 +6,5 @@ export default defineConfig({
   server: {
     port: 8080,
   },
-  plugins: [vue()],
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: `assets/[name].js`,
-        chunkFileNames: `assets/[name].js`,
-        assetFileNames: `assets/[name].[ext]`
-      }
-    }
-  }
+  plugins: [vue()]
 })
